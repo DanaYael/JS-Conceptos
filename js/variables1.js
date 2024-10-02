3e994e0690e2ab17a4ebf84e8a1d2260c9755127
Dana Yael Albornoz <Yaelalbornoz23@gmail.com>
2024-10-02 12:17:45 -0300

    tarea sobre Variables para el jueves 3/10

--- js/variables1.js
+++ js/variables1.js
@@ -1,3 +1,26 @@
-let edad = 20;
-console.log(edad);
-console.log("hola mundo");
+// Punto 1
+
+let num = 100;
+let resultado = num * 10;
+document.write("El resultado es: " + resultado + "<br>"); // Añadí un <br> para separar los resultados en la pantalla
+
+// Punto 2
+
+let tempF = 40;
+let conversionGradosC = (5 / 9) * (tempF - 32);
+console.log(
+  tempF +
+    "la conversion de fahrenheir a Ceslius es : " +
+    conversionGradosC +
+    " grados Celsius. " +
+    "<br>"
+);
+
+// Punto 3
+
+let largo = 20;
+let ancho = 60;
+let perimetro = "El perímetro es: " + 2 * (ancho + largo);
+let area = "El area es : " + ancho * largo;
+console.log(perimetro);
+console.log(area);
