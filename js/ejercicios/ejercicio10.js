3a0c2b579abfcdd690ccd125904127b3d45b738e
Dana Yael Albornoz <Yaelalbornoz23@gmail.com>
2024-10-08 13:31:31 -0300

    resolucion del ej 10

---
+++ js/ejercicios/ejercicio10.js
@@ -0,0 +1,6 @@
+const num = parseInt(prompt("ingrese un numero"));
+if (num % 2 === 0) {
+  document.write("el numero ingresado " + num + " es divisible por 2 ");
+} else {
+  document.write("el numero ingresado " + num + " no es divisible entre 2");
+}
