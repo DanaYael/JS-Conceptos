bff4e2a09a2c5b0cb32f31eb0a8901a61205725b
Dana Yael Albornoz <Yaelalbornoz23@gmail.com>
2024-10-09 20:37:17 -0300

    ejercicio Bucle 6 / piramide

---
+++ js/ejercicioBucles/ejercicio.B6.js
@@ -0,0 +1,9 @@
+// 6- Realiza un script que escriba una pirámide del 1 al 30
+
+for (let i = 1; i <= 30; i++) {
+  let fila = "";
+  for (let j = 1; j <= i; j++) {
+    fila += i;
+  }
+  console.log(fila);
+}
